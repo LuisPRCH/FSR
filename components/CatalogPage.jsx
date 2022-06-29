14cd2ed9f8a89559381afdd002860bffe953a6b7
Luis <eje@eje.com>
2022-06-29 08:36:16 -0600

    Add Catálogo Page

--- components/CatalogPage.jsx
+++ components/CatalogPage.jsx
@@ -1,7 +1,23 @@
-export default function CatalogPage() {
+import { Nav, Footer, HeaderOfCategory, CartOfProduct, Search } from './index'
+import { motion } from 'framer-motion'
+
+export default function CatalogPage({objectToUse}) {
 	return (
-		<div>
-			<h1>Luis</h1>
+		<div className='min-h-screen w-full absolute top-0 left-0 pb-[92px] md:pb-[55px]'>
+			<Nav />
+			<HeaderOfCategory title='Catálogo' />
+			<motion.main
+				animate={{ opacity: [0, 1], y: [50, 0] }}
+				className='opacity-0 w-full flex flex-col justify-start items-center gap-8 md:flex-row md:items-start md:justify-center md:gap-0'
+			>
+				<Search />
+				<section className='mb-8 px-8 flex flex-wrap w-full justify-center items-center gap-8'>
+					{objectToUse.map(producto => {
+						return <CartOfProduct productToUse={producto.node} />
+					})}
+				</section>
+			</motion.main>
+			<Footer />
 		</div>
 	)
 }
--- pages/catalogo/index.jsx
+++ pages/catalogo/index.jsx
@@ -4,5 +4,5 @@ import useQuery from '../../hooks/useQuery'
 export default function catalogo() {
 	const { allProducts } = useQuery()
 
-	return <>{!allProducts[0] ? null : <CatalogPage />}</>
+	return <>{!allProducts[0] ? null : <CatalogPage objectToUse={allProducts} />}</>
 }
