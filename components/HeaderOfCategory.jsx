c204cc02713a4fc5f417e82eacdbcaf4ecb9e642
Luis <eje@eje.com>
2022-07-05 10:02:14 -0600

    fix header img

--- components/HeaderOfCategory.jsx
+++ components/HeaderOfCategory.jsx
@@ -1,12 +1,11 @@
 import { motion } from 'framer-motion'
 
 export default function HeaderOfCategory({ title, img }) {
-	const back =
-		'opacity-0 mb-8 mt-[135px] md:mt-[92px] relative bg-[url("' + img + '")] bg-cover bg-left w-full h-[250px] flex justify-center items-center'
-
-	console.log(back)
 	return (
-		<motion.header animate={{ opacity: [0, 1], y: [50, 0] }} className={back}>
+		<motion.header
+			animate={{ opacity: [0, 1], y: [50, 0] }}
+			className='opacity-0 mb-8 mt-[135px] md:mt-[92px] relative bg-[url("https://media.graphassets.com/T04RbgjzSsScg07fziCF")] bg-cover bg-left w-full h-[250px] flex justify-center items-center'
+		>
 			<div className='w-full h-full absolute top-0 left-0 bg-gradient-to-r from-[#00000055] to-[#00000055] px-8 flex flex-col justify-center items-center'>
 				<motion.h1
 					transition={{ delay: 0.7, ease: 'easeInOut' }}
